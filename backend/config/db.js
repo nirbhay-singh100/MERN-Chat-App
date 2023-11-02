c8380ad5c5650c9ee3d9692eaa64f70d818c78ef
Nirbhay Singh <nirbhaymahan100@gmail.com>
2023-11-02 20:21:38 +0530

    final push

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,6 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+mongoose.connect('mongodb+srv://nirbhaymahan12:7k66833k96@cluster0.5ttr5oq.mongodb.net/ChatAPP', { useNewUrlParser: true, useUnifiedTopology: true});
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.bold);
-    process.exit(1); // Exit with a non-zero status code to indicate an error
-  }
-};
-
-module.exports = connectDB;
+// module.exports = connectDB;
 
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
 dotenv.config();
-connectDB();
+// connectDB();
 const app = express();
 
 app.use(express.json()); // to accept json data
@@ -23,19 +23,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
